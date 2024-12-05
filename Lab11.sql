5a1e9dde26d99c706deaf1798b9e8d4b202fd961
navjotk17 <navjot.mathoda17@gmail.com>
2024-12-05 12:43:40 -0500

    Add files via upload

---
+++ Lab11.sql
@@ -0,0 +1,33 @@
+-- TABLE
+CREATE TABLE assignments (  
+    id INTEGER PRIMARY KEY AUTOINCREMENT,  -- Unique ID for each assignment  
+    course_id TEXT NOT NULL,  -- Foreign key referencing courses  
+    title TEXT NOT NULL,
+    status TEXT NOT NULL,  -- Status of the assignment (e.g., "Not Started", "In Progress", "Completed") 
+    due_date TEXT,  -- Due date stored as a string (e.g., "YYYY-MM-DD")  
+    FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE  
+);
+CREATE TABLE courses (  
+    course_id TEXT PRIMARY KEY,  -- String ID for courses, e.g., "COMP1234"  
+    semester TEXT NOT NULL,  -- Semester the course is offered (e.g., "2024-3" for Fall 2024)
+    course_name TEXT NOT NULL,
+    lab_time TEXT,  -- Time of the lab session (e.g., "Tue 10")
+    lecture_time TEXT,  -- Time of the lecture session (e.g., "Mon 13")
+    notes TEXT  
+);
+CREATE TABLE rectangles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    -- top left corner coordinates
+    x REAL NOT NULL,
+    y REAL NOT NULL,
+    width REAL NOT NULL,
+    height REAL NOT NULL,
+    color TEXT -- css style color
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
